f07fe22f88fbbc7d788598ae2e5ecf61f96c2793
Julian Müller <julimueller1998@gmail.com>
2019-12-30 18:36:08 +0100

    fix harvester behavior

--- dist/roleHarvester.js
+++ dist/roleHarvester.js
@@ -2,10 +2,14 @@ var roleHarvester = {
 
   /** @param {Creep} creep **/
   run: function(creep) {
-	   if(creep.store.getUsedCapacity() == 0) {
+	  if(creep.store.getUsedCapacity() == 0 || creep.memory.harvesting) {
+      creep.memory.harvesting = true;
       var source = Game.getObjectById(creep.memory.sourceId);
       if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
           creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
+      }
+      if(creep.store.getFreeCapacity() == 0) {
+        creep.memory.harvesting = false;
       }
     }
     else {
