bd5c9fcef20fe5273b5a0efed61ec729ce96a2ac
Julian Müller <julimueller1998@gmail.com>
2019-12-30 17:04:47 +0100

    Move builder away if no work hast to be done

--- dist/roleBuilder.js
+++ dist/roleBuilder.js
@@ -2,7 +2,6 @@ var roleBuilder = {
 
   /** @param {Creep} creep **/
   run: function(creep) {
-
     if(creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
       creep.memory.building = false;
       creep.say('🔄 harvest');
@@ -18,6 +17,9 @@ var roleBuilder = {
         if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
           creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
         }
+      }
+      else {
+        creep.moveTo(Game.spawns["Spawn1"].pos);
       }
     }
     else {
